48a48e52106f6fc1798ecd2a027549a27adbc8c8
sercansoft <sercan.demir@boun.edu.tr>
2020-12-19 17:49:26 +0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,7 @@
         </style>
     </head>
     <body>
+    test
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
